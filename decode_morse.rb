2dbced1ec9d175084e03ab2358ad491c5c5eb3f3
Oluyemi Paul <oluola96@gmail.com>
2022-10-27 17:18:58 +0100

    Update decode_morse.rb

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,23 +1,23 @@
 def decode(code)
-  char_Translator = {
-      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
-      '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
-      '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-      '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
-      '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
-      '-.--' => 'Y', '--..' => 'Z'
-    }
-  char_Translator[code]
+  char_translator = {
+                      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+                      '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+                      '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+                      '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+                      '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+                      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+                      '-.--' => 'Y', '--..' => 'Z'
+                    }
+  char_translator[code]
 end
-  
+
 def decode_word(decode_word)
   decode_word.split.map { |char| decode(char) }.join
 end
-  
+
 def decoder(decode_sentence)
   decode_sentence.split('   ').map { |word| decode_word(word) }.join(' ')
 end
-  
+
 print decoder('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
 # Prints: "A BOX FULL OF RUBIES"
